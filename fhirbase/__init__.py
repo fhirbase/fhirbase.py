a35e25179d42f6365d9ff8ec2586751e075b55d1
ruscoder <rusmiligamer@gmail.com>
2018-08-28 17:50:51 +0700

    Replace implementation with fn wrapper

--- fhirbase/__init__.py
+++ fhirbase/__init__.py
@@ -1,49 +1,85 @@
-import json
-
-from psycopg2.extensions import AsIs
-from uuid import uuid4
+from psycopg2 import DatabaseError
+from psycopg2.extras import Json
 
 __version__ = '0.0.1'
+
+
+def get_ref(*args):
+    if len(args) == 1 and isinstance(args[0], dict):
+        resource = args[0]
+
+        resource_type = resource.get('resourceType', None)
+        resource_id = resource.get('id', None)
+
+        if not resource_type or not resource_id:
+            raise TypeError(
+                'First argument must contain `resourceType` and `id` keys')
+    elif len(args) == 2:
+        resource_type = args[0]
+        resource_id = args[1]
+    else:
+        raise TypeError('Resource type and id is required')
+
+    return resource_type, resource_id
 
 
 class FHIRBaseConnection(object):
     def __init__(self, connection):
         self.connection = connection
 
-    def create_resource(self, txid, **resource):
-        try:
-            resource_type = resource['resourceType']
-        except KeyError:
-            raise TypeError(
-                '`resource` must contain `resourceType`')
-
-        resource_id = resource.pop('id', None) or str(uuid4())
-
+    def _execute(self, sql, params):
         cursor = self.connection.cursor()
 
-        cursor.execute(
-            '''
-            INSERT INTO %(table)s 
-                (
-                    id, ts, txid, status, resource_type, resource
-                )
-            VALUES 
-                (
-                    %(id)s, current_timestamp, %(txid)s, 'created', 
-                    %(resource_type)s, %(resource)s
-                )
-            ON CONFLICT (id) DO UPDATE SET
-                txid = %(txid)s, 
-                ts = current_timestamp, 
-                status = 'recreated',
-                resource = %(resource)s
-            ''',
-            {
-                'table': AsIs(resource_type.lower()),
-                'id': resource_id,
-                'txid': txid,
-                'resource_type': resource_type,
-                'resource': json.dumps(resource),
-            }
+        try:
+            cursor.execute(sql, params)
+            self.connection.commit()
+
+            return cursor.fetchone()[0]
+        except DatabaseError:
+            self.connection.rollback()
+            raise
+        finally:
+            cursor.close()
+
+    def _execute_fn(self, fn_name, params, txid=None):
+        params = params.copy()
+
+        if txid:
+            params.append(txid)
+
+        sql = 'SELECT fhirbase_{fn}({params})'.format(
+            fn=fn_name,
+            params=','.join(['%s'] * len(params))
         )
-        self.connection.commit()
+
+        return self._execute(sql, params)
+
+    def start_transaction(self, info=None):
+        info = info or {}
+
+        return self._execute(
+            'INSERT INTO transaction (resource) VALUES (%s) RETURNING id',
+            [Json(info)]
+        )
+
+    def create(self, resource, *, txid=None):
+        resource_type = resource.get('resourceType', None)
+        if not resource_type:
+            raise TypeError('`resource` must contain `resourceType` key')
+
+        return self._execute_fn('create', [Json(resource)], txid)
+
+    def update(self, resource, *, txid=None):
+        resource_type, _ = get_ref(resource)
+
+        return self._execute_fn('update', [Json(resource)], txid)
+
+    def read(self, *args):
+        resource_type, resource_id = get_ref(*args)
+
+        return self._execute_fn('read', [resource_type, resource_id])
+
+    def delete(self, *args, txid=None):
+        resource_type, resource_id = get_ref(*args)
+
+        return self._execute_fn('delete', [resource_type, resource_id], txid)
