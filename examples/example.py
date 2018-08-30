3fa2c7725cb023583ec0b828ed743e25bfd1d32b
ruscoder <rusmiligamer@gmail.com>
2018-08-30 10:13:03 +0700

    Change travis settings

---
+++ examples/example.py
@@ -0,0 +1,34 @@
+import os
+import psycopg2
+import fhirbase
+
+
+def db_connect():
+    pguser = os.getenv('PGUSER', 'postgres')
+    pgpassword = os.getenv('PGPASSWORD', 'postgres')
+    pghost = os.getenv('PGHOST', 'localhost')
+    pgport = os.getenv('PGPORT', '5432')
+    dbname = os.getenv('PGDATABASE', 'postgres')
+
+    return psycopg2.connect(dbname=dbname, user=pguser,
+                            password=pgpassword, host=pghost, port=pgport)
+
+
+if __name__ == '__main__':
+    conn = db_connect()
+    try:
+        fb = fhirbase.FHIRBase(conn)
+
+        patient = fb.create({'resourceType': 'Patient'})
+        print('Created patient: ', patient)
+
+        patient.update({'name': [{'text': 'John'}]})
+        updated_patient = fb.update(patient)
+        print('Updated patient: ', updated_patient)
+
+        fetched_patient = fb.read(patient)
+        print('Fetched patient:', fetched_patient)
+
+        fb.delete(patient)
+    finally:
+        conn.close()
--- tests/test_fhirbase.py
+++ tests/test_fhirbase.py
@@ -1,1 +1,30 @@
+import os
 
+import psycopg2
+import pytest
+
+import fhirbase
+
+
+@pytest.fixture
+def db():
+    pguser = os.getenv('PGUSER', 'postgres')
+    pgpassword = os.getenv('PGPASSWORD', 'postgres')
+    pghost = os.getenv('PGHOST', 'localhost')
+    pgport = os.getenv('PGPORT', '5432')
+    dbname = os.getenv('PGDATABASE', 'fhirbase')
+
+    return psycopg2.connect(dbname=dbname, user=pguser,
+                            password=pgpassword, host=pghost, port=pgport)
+
+
+def test_connect(db):
+    cur = db.cursor()
+    cur.close()
+
+
+def test_create(db):
+    fb = fhirbase.FHIRBase(db)
+    patient = fb.create({'resourceType': 'Patient'})
+    if patient.get('id', None) is None:
+        pytest.fail('Just created patient should have id')
