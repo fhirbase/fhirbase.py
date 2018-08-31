e0997f647149ce0ccc93d82bb93d787f63f5e9a8
ruscoder <rusmiligamer@gmail.com>
2018-08-31 21:42:16 +0700

    Include docker-compose.override.yml in tests

--- run_tests.sh
+++ run_tests.sh
@@ -4,8 +4,8 @@ set -e
 mkdir -p shared
 docker-compose up -d
 sleep 1  # I don't know why, but it solves a problem with DNS
-docker-compose -f docker-compose.yml -f docker-compose.test.yml run --rm wait_for
-docker-compose -f docker-compose.yml -f docker-compose.test.yml run --rm db fhirbase init 3.0.1
-docker-compose -f docker-compose.yml -f docker-compose.test.yml run --rm tests $@
+docker-compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.test.yml run --rm wait_for
+docker-compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.test.yml run --rm db fhirbase init 3.0.1
+docker-compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.test.yml run --rm tests $@
 
 docker-compose down
